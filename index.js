32e75d0165b131045107045e0604670c6371376a
Richard Marmorstein <richard.marmorstein@livestream.com>
2016-12-27 18:36:39 -0500

    Allow to be included as a module

--- index.js
+++ index.js
@@ -6,9 +6,20 @@ const WebSocketServer = require('ws').Server
 const Room = require('./lib/room')
 const Wsss = require('./lib/wsss')
 
-// App bootstrap
-const port = process.argv.length > 2 && process.argv[2] || 8000
-console.log(`Starting websocket server on port ${port}`)
-const wss = new WebSocketServer({port})
+const noopLogger = {
+    info: ()=>{}
+}
+const stdoutLogger = {
+    info: console.log
+}
+function init (port, logger) {
+    logger.info(`Starting websocket server on port ${port}`)
+    const wss = new WebSocketServer({port})
+    Wsss.init(Room, logger, wss)
+}
 
-Wsss.init(Room, wss)
+module.exports = init
+if (require.main === module) {
+    const port = process.argv.length > 2 && process.argv[2] || 8000
+    init(port, stdoutLogger)
+}
